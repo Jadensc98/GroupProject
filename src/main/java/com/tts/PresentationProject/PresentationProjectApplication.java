4770ce24d56e00bfe7ea1ab0f3dfc39043dfeab8
Jaden <jadenscott98@yahoo.com>
2018-12-14 15:21:02 -0500

    first

---
+++ src/main/java/com/tts/PresentationProject/PresentationProjectApplication.java
@@ -0,0 +1,14 @@
+package com.tts.PresentationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentationProjectApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tts/PresentationProject/PresentationProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tts.PresentationProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PresentationProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
