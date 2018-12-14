b8367b0e2cea2ee04a81a2f7d0f1654ab05c9e76
Jaden <jadenscott98@yahoo.com>
2018-12-14 16:43:21 -0500

    added stuff in comments model

--- src/main/java/com/tts/PresentationProject/BlogPost/BlogPost.java
+++ src/main/java/com/tts/PresentationProject/BlogPost/BlogPost.java
@@ -1,9 +1,11 @@
 package com.tts.PresentationProject.BlogPost;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class BlogPost {
