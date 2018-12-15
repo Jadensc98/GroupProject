c1c99b28c47b217f84952ed45713d59aca28a0a9
Qazwsxedcr <greenladybug172@gmail.com>
2018-12-15 17:33:45 -0500

    Add User attributes to the user class

--- src/main/java/com/tts/PresentationProject/BlogPost/User.java
+++ src/main/java/com/tts/PresentationProject/BlogPost/User.java
@@ -1,5 +1,16 @@
 package com.tts.PresentationProject.BlogPost;
 
-public class User {
+import java.util.Date;
 
+public class User {
+    private Long id;
+    String firstName;
+    String lastName;
+    String userName;
+    String  email;
+    
+    //(date of creation or last update date of account)
+    private Date updateDttm;
+    //private Date;
+    String password;
 }
