f24a2c97c125a211a78122fdd4572c326f3721ba
clickadawrist <shiningsprites@gmail.com>
2018-12-16 18:28:38 -0500

    Get rid of attributes and import that I had typed in so that it will not interfere with Sneha's implementation

--- src/main/java/com/tts/PresentationProject/BlogPost/User.java
+++ src/main/java/com/tts/PresentationProject/BlogPost/User.java
@@ -1,16 +1,5 @@
 package com.tts.PresentationProject.BlogPost;
 
-import java.util.Date;
-
 public class User {
-    private Long id;
-    String firstName;
-    String lastName;
-    String userName;
-    String  email;
-    
-    //(date of creation or last update date of account)
-    private Date updateDttm;
-    //private Date;
-    String password;
+
 }
