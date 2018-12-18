d14cebcdee9b66d6d1941d90b337a8f224c4c02e
Jaden <jadenscott98@yahoo.com>
2018-12-18 13:36:13 -0500

    made user model and repo

--- src/main/java/com/tts/PresentationProject/BlogPost/User.java
+++ src/main/java/com/tts/PresentationProject/BlogPost/User.java
@@ -1,5 +1,47 @@
 package com.tts.PresentationProject.BlogPost;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		private Long id;
+		private String userName;
+		private String passWord;
+		
+		
+		
+		public User() {
+			
+		}
+		
+		public User(String userName, String passWord) {
+			this.userName = userName;
+			this.passWord = passWord;
+
+		}
+		
+		
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getPassWord() {
+			return passWord;
+		}
+		public void setPassWord(String passWord) {
+			this.passWord = passWord;
+		}
+		@Override
+		public String toString() {
+			return "UserRepository [userName=" + userName + ", passWord=" + passWord + "]";
+		}
+		
 }
---
+++ src/main/java/com/tts/PresentationProject/BlogPost/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tts.PresentationProject.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+	public interface UserRepository extends CrudRepository<User, Long> {
+		
+
+	}
+
+
